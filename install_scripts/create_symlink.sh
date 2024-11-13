394c5a0c5bb4ecce85cd1ebe7c0b6a7872b13e1e
NT-marlowe <n.tezuka196.ninj@gmail.com>
2024-11-13 18:46:22 +0900

    update

--- install_scripts/create_symlink.sh
+++ install_scripts/create_symlink.sh
@@ -5,16 +5,7 @@ cd ~
 ln -s ~/dotfiles/.zshrc .
 ln -s ~/dotfiles/.zshrc ./.zlogin
 
-mkdir -p ~/.config
-ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
-
-mkdir -p ~/.config/wezterm
-ln -s ~/dotfiles/wezterm.lua ~/.config/wezterm/wezterm.lua
-
-mkdir -p ~/.config/nvim
-ln -s ~/dotfiles/init.lua ~/.config/nvim/init.lua
-ln -s ~/dotfiles/lua/ ~/.config/nvim/lua
+ln -s ~/dotfiles/.config ~/
 
 ln -s ~/dotfiles/.fzf.zsh ~/.fzf.zsh
 
-ln -s ~/dotfiles/yazi ~/.config/
