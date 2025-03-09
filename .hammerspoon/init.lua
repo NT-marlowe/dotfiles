f0eb431c640ffd669ff789008ddea689c20e6c9c
NT-marlowe <n.tezuka196.ninj@gmail.com>
2025-03-09 20:27:54 +0900

    split files

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,30 +1,4 @@
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
-local function keyCode(key, modifiers)
-    modifiers = modifiers or {}
-    return function()
-       hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
-       hs.timer.usleep(1000)
-       hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
-    end
- end
-
-local function remapKey(modifiers, key, keyCode)
-   hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
-end
-
-remapKey({'ctrl'}, 'h', keyCode('left'))
-remapKey({'ctrl'}, 'j', keyCode('down'))
-remapKey({'ctrl'}, 'k', keyCode('up'))
-remapKey({'ctrl'}, 'l', keyCode('right'))
-
-remapKey({'ctrl', 'cmd'}, 'h', keyCode('left', {'cmd'}))
-remapKey({'ctrl', 'cmd'}, 'j', keyCode('down', {'cmd'}))
-remapKey({'ctrl', 'cmd'}, 'k', keyCode('up', {'cmd'}))
-remapKey({'ctrl', 'cmd'}, 'l', keyCode('right', {'cmd'}))
-
-remapKey({'ctrl', 'alt'}, 'h', keyCode('left', {'alt'}))
-remapKey({'ctrl', 'alt'}, 'j', keyCode('down', {'alt'}))
-remapKey({'ctrl', 'alt'}, 'k', keyCode('up', {'alt'}))
-remapKey({'ctrl', 'alt'}, 'l', keyCode('right', {'alt'}))
+require('lua.keymaps')
